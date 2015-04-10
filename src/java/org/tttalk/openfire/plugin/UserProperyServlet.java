b7f6c272debe35fe89288af6b91bcb6907251e2a
oizhaolei <oizhaolei@gmail.com>
2015-04-10 08:40:52 +0800

    user properties

---
+++ src/java/org/tttalk/openfire/plugin/UserProperyServlet.java
@@ -0,0 +1,47 @@
+package org.tttalk.openfire.plugin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ */
+public class UserProperyServlet extends AbstractTranslatorServlet {
+	private static final long serialVersionUID = -961330208339141998L;
+	private static final Logger Log = LoggerFactory
+			.getLogger(UserProperyServlet.class);
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		Log.info(request.toString());
+
+		String username = request.getParameter("user");
+		String key = request.getParameter("key");
+		String value = request.getParameter("value");
+		PrintWriter out = response.getWriter();
+		try {
+			if (value == null) {
+				value = plugin.getProperty(username, key);
+				out.println(value);
+			} else {
+				plugin.updateProperty(username, key, value);
+				out.println("success");
+			}
+		} catch (Exception e) {
+			out.println(e.getMessage());
+		}
+	}
+
+	@Override
+	String getUri() {
+		return "/userproperties";
+	}
+}
